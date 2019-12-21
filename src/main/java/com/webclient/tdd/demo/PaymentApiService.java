cb01fabc828548ac2c6b5003991148594ca0150d
Enrique Ell Pereira <enrique.ell@gmail.com>
2019-12-21 19:52:21 -0300

    Should return AuthenticationToken

--- src/main/java/com/webclient/tdd/demo/PaymentApiService.java
+++ src/main/java/com/webclient/tdd/demo/PaymentApiService.java
@@ -1,14 +1,30 @@
 package com.webclient.tdd.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
-public class PaymentApi {
+public class PaymentApiService {
 
+    final
     WebClient paymentApiWebClient;
 
-    public String getAuthenticationToken() {
-        return null;
+    public PaymentApiService(WebClient paymentApiWebClient) {
+        this.paymentApiWebClient = paymentApiWebClient;
+    }
+
+    @Bean
+    public static WebClient getPaymentApiWebClient() {
+        return WebClient.create("https://www.google.com");
+    }
+
+    public Mono<String> getAuthenticationToken() {
+        return paymentApiWebClient
+                .post()
+                .uri("/api/authentication")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
--- src/test/java/com/webclient/tdd/demo/PaymentApiServiceTest.java
+++ src/test/java/com/webclient/tdd/demo/PaymentApiServiceTest.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-public class PaymentApiTest {
+public class PaymentApiServiceTest {
 
     @Autowired
-    PaymentApi paymentApi;
+    PaymentApiService paymentApiService;
 
     @MockBean
     WebClient paymentApiWebClient;
@@ -36,16 +36,19 @@ public class PaymentApiTest {
     @Test
     public void shouldGetAuthenticationToken() {
         String tokenExpected = "jhaskjdhlas=sadklasd";
-
-        when(paymentApiWebClient.post()).thenReturn(requestBodyUri);
+        when(paymentApiWebClient.post())
+                .thenReturn(requestBodyUri);
         when(requestBodyUri.uri("/api/authentication")).
                 thenReturn(requestHeadersUriSpecMock);
-        when(requestHeadersUriSpecMock.retrieve()).thenReturn(responseSpekMock);
-        when(responseSpekMock.bodyToFlux(String.class)).thenReturn(Flux.just(tokenExpected));
+        when(requestHeadersUriSpecMock.retrieve())
+                .thenReturn(responseSpekMock);
+        when(responseSpekMock.bodyToMono(String.class))
+                .thenReturn(Mono.just(tokenExpected));
 
-        String token = paymentApi.getAuthenticationToken();
+        Mono<String> token = paymentApiService.getAuthenticationToken();
 
-        assertEquals(tokenExpected, token);
+        assertEquals(tokenExpected, token.block());
     }
 
+    
 }
