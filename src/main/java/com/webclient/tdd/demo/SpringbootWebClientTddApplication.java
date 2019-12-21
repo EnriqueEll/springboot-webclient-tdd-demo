344831addc43db7295800a177e209088291f9a70
Enrique Ell Pereira <enrique.ell@gmail.com>
2019-12-21 18:44:01 -0300

    Initial commit

---
+++ src/main/java/com/webclient/tdd/demo/SpringbootWebClientTddApplication.java
@@ -0,0 +1,13 @@
+package com.webclient.tdd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebClientTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebClientTddApplication.class, args);
+    }
+
+}
